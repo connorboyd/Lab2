f6b9acae4b6bd7ea75a45f776e413d2b241a4dfb
connorboyd <connor.boyd@spir3.com>
2013-09-11 18:08:40 -0600

    Added Lab2.rb

---
+++ Lab2.rb
@@ -0,0 +1,11 @@
+# Part1: Hello World
+class HelloWorldClass
+	def initialize(name)
+		@name = name.capitalize
+	end
+	def sayHi
+		puts "Hello #{@name}!"
+	end
+end
+hellow = HelloWorldClass.new("{type your name here}"_
+hello.sayHi
